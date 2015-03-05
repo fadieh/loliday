0fc782a0f240ec14fba46e161a29208412716933
Fadie Hannona <fadiehannona@gmail.com>
2015-03-05 15:42:19 +0000

    fixed hover menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 	$(".menu-hover").hover(function(){
-		$(this).css("border-bottom", "2px solid white")
-		$(this).css("padding-bottom", "-3px")
+		$(this).css("border-bottom", "1.5px solid white")
+		$(this).css("padding-bottom", "6px")
 		$(this).css("color", "white")
 	},
 	function(){
 		$(this).css("border-bottom", "none")
 		$(this).css("padding-bottom", 0)
+		$(this).css("color", "#A5A5A5")
 	});
 })
